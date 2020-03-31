2bd767a70747c926314e3be7a83b55ab651b49cd
EvelinaChumak <45465788+EvelinaChumak@users.noreply.github.com>
2020-03-31 20:48:33 +0300

    added read and output image

---
+++ ImgToASCII/Source.cpp
@@ -0,0 +1,39 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+
+int main(int argc, char** argv)
+{
+
+
+	if (argc != 2)
+	{
+		cout << " Usage: display_image ImageToLoadAndDisplay" << endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);   // Read the file
+
+	if (!image.data)                              // Check for invalid input
+	{
+		cout << "Could not open or find the image" << std::endl;
+		return -1;
+	}
+	// We iterate over all pixels of the image
+
+	for (int r = 0; r < image.rows; r++) {
+		cout << int(image.at<uchar>(r, image.cols / 2)) << ' ';
+	}
+
+	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
+	imshow("Display window", image);                   // Show our image inside it.
+
+	waitKey(0);                                          // Wait for a keystroke in the window
+	return 0;
+}
