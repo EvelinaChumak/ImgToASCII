fca2ca1bd349930a8c28ec1c222e85cfde317d31
EvelinaChumak <45465788+EvelinaChumak@users.noreply.github.com>
2020-04-06 20:03:09 +0300

    first versian

--- ImgToASCII/Source.cpp
+++ ImgToASCII/Source.cpp
@@ -2,13 +2,14 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include "DIctionary.h"
+#include <fstream>
 using namespace cv;
 using namespace std;
 
 
 int main(int argc, char** argv)
 {
-
+	ofstream f("text.txt", ios_base::out);
 
 	if (argc != 2)
 	{
@@ -24,14 +25,49 @@ int main(int argc, char** argv)
 		cout << "Could not open or find the image" << std::endl;
 		return -1;
 	}
+	for (int r = 0; r < image.rows - 10; r=r + 10)
+	{
+		for (int c = 0; c < image.cols - 10; c = c + 10)
+		{
+			string s;
+			for (int i = r; i < r + 10; i = i+2)
+			{
+				int rem = 0;
+				for (int j = c; j < c + 10; j = j+2)
+				{
+					if (rem + int(image.at<uchar>(i, j)) > 127)
+					{
+						s = s + '1';
+						rem = rem - (255 - int(image.at<uchar>(i, j)));
+					 }
+					else
+					{
+						s = s + '0';
+						rem = rem + int(image.at<uchar>(i, j));
+					}
+				}
+			}
+			auto it = dict.find(s);
+			if (it != dict.end())
+				f << (*it).second;
+			else
+			{
+				auto z = dict.lower_bound(s);
+				f << (*z).second;
+			}
+		}
+		f << endl;
+	}
 	/*
 	for (int r = 0; r < image.rows; r++) {
 		cout << int(image.at<uchar>(r, image.cols / 2)) << ' ';
 	}
 	*/
-	namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
-	imshow("Display window", image);                   // Show our image inside it.
 
-	waitKey(0);                                         
+	f.close();
+	//namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
+	//imshow("Display window", image);                   // Show our image inside it.
+
+	//waitKey(0);                                         
 	return 0;
 }
